281abd2498bb594e101111d87d7906bcab94b15a
parkingwang <zhangliping@parkingwang.com>
2017-10-03 00:09:59 +0800

    1003-first

--- app/navg_flask.py
+++ app/navg_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request,render_template
 
 app = Flask(__name__)
 
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def navg_flask(name):
     return '<h1> hello %s welcome to navg<h1>' %name
 
+@app.route('/test/<name>')
+def set_render_template(name):
+    return render_template('user.html',name=name)
 
 
 if __name__ == '__main__':
