c70e28c045f403e38a34ccc6e44cce9bd4327f04
952556471 <952556471@qq.com>
2020-04-25 19:06:41 +0800

    基础化master分支内容

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/
 # -*- coding:utf-8 -*-
-
 from flask import Blueprint
 
-main = Blueprint('main', __name__)
+auth = Blueprint('auth', __name__)
 
-from . import views, errors
+from . import views
