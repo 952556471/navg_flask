2c7de2529c4e974839c0376bbb9ccd1dd67bee0b
952556471 <952556471@qq.com>
2020-04-25 19:20:26 +0800

    基础化huabingecost分支内容

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python2.7
-# -*- coding:utf-8 -*-
-
 from flask import Blueprint
 
-main = Blueprint('main',__name__)
+main = Blueprint('main', __name__)
 
-from . import navg_flask
+from . import views, errors
